178b890252175a59c9070229ebe372b62ede9e3b
Minijackson <minijackson@riseup.net>
2025-07-03 09:17:47 +0200

    fix: fix rust bindings

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -1,4 +1,4 @@
-//! This crate provides EpicsMsiTemplate language support for the [tree-sitter] parsing library.
+//! This crate provides EPICS MSI template (macro) language support for the [tree-sitter] parsing library.
 //!
 //! Typically, you will use the [`LANGUAGE`] constant to add this language to a
 //! tree-sitter [`Parser`], and then use the parser to parse some code:
@@ -7,10 +7,10 @@
 //! let code = r#"
 //! "#;
 //! let mut parser = tree_sitter::Parser::new();
-//! let language = tree_sitter_epics-msi-template::LANGUAGE;
+//! let language = tree_sitter_epics_msi_template::LANGUAGE;
 //! parser
 //!     .set_language(&language.into())
-//!     .expect("Error loading EpicsMsiTemplate parser");
+//!     .expect("Error loading EPICS MSI template parser");
 //! let tree = parser.parse(code, None).unwrap();
 //! assert!(!tree.root_node().has_error());
 //! ```
@@ -21,20 +21,18 @@
 use tree_sitter_language::LanguageFn;
 
 extern "C" {
-    fn tree_sitter_epics-msi-template() -> *const ();
+    fn tree_sitter_epics_msi_template() -> *const ();
 }
 
 /// The tree-sitter [`LanguageFn`] for this grammar.
-pub const LANGUAGE: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_epics-msi-template) };
+pub const LANGUAGE: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_epics_msi_template) };
 
 /// The content of the [`node-types.json`] file for this grammar.
 ///
 /// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers/6-static-node-types
 pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
 
-// NOTE: uncomment these to include any queries that this grammar contains:
-
-// pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
+pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
 // pub const INJECTIONS_QUERY: &str = include_str!("../../queries/injections.scm");
 // pub const LOCALS_QUERY: &str = include_str!("../../queries/locals.scm");
 // pub const TAGS_QUERY: &str = include_str!("../../queries/tags.scm");
@@ -46,6 +44,6 @@ mod tests {
         let mut parser = tree_sitter::Parser::new();
         parser
             .set_language(&super::LANGUAGE.into())
-            .expect("Error loading EpicsMsiTemplate parser");
+            .expect("Error loading EPICS MSI template parser");
     }
 }
