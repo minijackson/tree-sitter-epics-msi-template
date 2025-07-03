f900328a551f7cbbde0b18c81a315df06c5cfc8e
Minijackson <minijackson@riseup.net>
2025-07-03 09:36:18 +0200

    fix(node): fix node bindings

--- binding.gyp
+++ binding.gyp
@@ -1,7 +1,7 @@
 {
   "targets": [
     {
-      "target_name": "tree_sitter_epics-msi-template_binding",
+      "target_name": "tree_sitter_epics_msi_template_binding",
       "dependencies": [
         "<!(node -p \"require('node-addon-api').targets\"):node_addon_api_except",
       ],
--- bindings/node/binding.cc
+++ bindings/node/binding.cc
@@ -2,7 +2,7 @@
 
 typedef struct TSLanguage TSLanguage;
 
-extern "C" TSLanguage *tree_sitter_epics-msi-template();
+extern "C" TSLanguage *tree_sitter_epics_msi_template();
 
 // "tree-sitter", "language" hashed with BLAKE2
 const napi_type_tag LANGUAGE_TYPE_TAG = {
@@ -10,10 +10,10 @@ const napi_type_tag LANGUAGE_TYPE_TAG = {
 };
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
-    auto language = Napi::External<TSLanguage>::New(env, tree_sitter_epics-msi-template());
+    auto language = Napi::External<TSLanguage>::New(env, tree_sitter_epics_msi_template());
     language.TypeTag(&LANGUAGE_TYPE_TAG);
     exports["language"] = language;
     return exports;
 }
 
-NODE_API_MODULE(tree_sitter_epics-msi-template_binding, Init)
+NODE_API_MODULE(tree_sitter_epics_msi_template_binding, Init)
